6edcd38d4b5e8a2c00705e5fd0900ccc4ae26d5f
mailchecker <zinux@naver.com>
2024-01-02 10:40:43 +0900

    Update gsheet_app.py

--- gsheet_app.py
+++ gsheet_app.py
@@ -1,23 +1,23 @@
-#import streamlit as st
-#from streamlit_gsheets import GSheetsConnection
-
-# Create a connection object.
-#conn = st.connection("gsheets", type=GSheetsConnection)
-
-#df = conn.read()
-
-# Print results.
-#for row in df.itertuples():
-#    st.write(f"{row.name} has a :{row.date}:")
-
-
-
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
-
+# Create a connection object.
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+df = conn.read()
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.date}:")
+
+
+
+#import streamlit as st
+#from streamlit_gsheets import GSheetsConnection
+
+#url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+
+#conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+#data = conn.read(spreadsheet=url, usecols=[0, 1])
+#st.dataframe(data)
