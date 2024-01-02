19e414b48fd6d496889c69003fa91c9746c2cc6c
mailchecker <zinux@naver.com>
2024-01-03 08:09:09 +0900

    Update gsheet_app.py

--- gsheet_app.py
+++ gsheet_app.py
@@ -5,9 +5,8 @@ st.title("Read Google Sheet as DataFrame")
 
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-sheet_data = [{'Timestamp': '2024-01-03', 'User_Input': 'input', 'User_Output': 'output'}]
-conn.update(worksheet="sheet1", data=sheet_data)
-
+#sheet_data = [{'Timestamp': '2024-01-03', 'User_Input': 'input', 'User_Output': 'output'}]
+#conn.update(worksheet="sheet1", data=sheet_data)
 
 df = conn.read(worksheet="sheet1")
 st.dataframe(df)
