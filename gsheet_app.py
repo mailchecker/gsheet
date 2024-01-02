d8006efdb207948c1b0960e9bab3495a9ee5a11f
mailchecker <zinux@naver.com>
2024-01-02 10:29:51 +0900

    Update gsheet_app.py

--- gsheet_app.py
+++ gsheet_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
-# conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.connection("gsheets", type=GSheetsConnection)
 
-# df = conn.read()
+df = conn.read()
 
 # Print results.
-# for row in df.itertuples():
-#     st.write(f"{row.장소} has a :{row.일자}:")
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.date}:")
