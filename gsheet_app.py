d0dba987a049c540c0912024268a674000488e60
mailchecker <zinux@naver.com>
2024-01-02 10:33:31 +0900

    Update gsheet_app.py

--- gsheet_app.py
+++ gsheet_app.py
@@ -1,11 +1,23 @@
+#import streamlit as st
+#from streamlit_gsheets import GSheetsConnection
+
+# Create a connection object.
+#conn = st.connection("gsheets", type=GSheetsConnection)
+
+#df = conn.read()
+
+# Print results.
+#for row in df.itertuples():
+#    st.write(f"{row.name} has a :{row.date}:")
+
+
+
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-# Create a connection object.
-conn = st.connection("gsheets", type=GSheetsConnection)
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
-df = conn.read()
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.date}:")
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
